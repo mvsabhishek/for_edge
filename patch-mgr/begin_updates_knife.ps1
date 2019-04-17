57c97f8831c2b5d33175db418adb4b0c42ebe964
Venkat Munukutla <mvsabhishek@gmail.com>
2019-04-17 19:37:59 +0000

    updated powershell scripts

--- patch-mgr/begin_updates_knife.ps1
+++ patch-mgr/begin_updates_knife.ps1
@@ -4,5 +4,5 @@ For($i = 0; $i -lt $nodes.Length; $i++){
     knife node run_list add $nodes[$i] 'recipe[update_windows]' | Add-Content C:\chef-repo\log.txt| Out-Null
     #knife winrm $nodes[$i] 'chef-client' --winrm-user siva.munukutla@cheftest.edgenuity.com --winrm-password Jaihanuman#1234
     $temp = "$($nodes[$i]).cheftest.edgenuity.com"
-    Invoke-Command -ComputerName $temp -ScriptBlock {chef-client} |Out-Null
+    Invoke-Command -ComputerName $temp -ScriptBlock {chef-client} | Add-Content C:\chef-repo\log.txt| Out-Null
 }
