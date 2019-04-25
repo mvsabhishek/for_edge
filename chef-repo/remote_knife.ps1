68dced2786d7052ea169ca4c74384d4446b78170
Venkat Munukutla <mvsabhishek@gmail.com>
2019-04-25 15:42:12 +0000

    Added cookbooks and changed folder structure

---
+++ chef-repo/remote_knife.ps1
@@ -0,0 +1,14 @@
+﻿[CmdletBinding()]
+Param(
+ $node
+)
+<#
+$username = "cheftest.edgenuity.com\siva.munukutla"
+$password = "Jaihanuman#1234"
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+#>
+cd C:\chef-repo
+knife node run_list add $node 'recipe[update_windows]' | Add-Content C:\chef-repo\log.txt| Out-Null
+#knife winrm "node:$node" 'chef-client' --winrm-user siva.munukutla@cheftest.edgenuity.com --winrm-password Jaihanuman#1234
+$temp = "$node.cheftest.edgenuity.com" 
+Invoke-Command -ComputerName $temp -ScriptBlock {chef-client} -Authentication Negotiate | Add-Content C:\chef-repo\log.txt | Out-Null
